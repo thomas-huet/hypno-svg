5bbdf1ad8ddd4df7fc0432c48355ad47b4ddd5ce
Thomas HUET <git@thomash.fr>
2019-02-13 11:23:57 +0100

    Color primes differently

--- make_svg.ml
+++ make_svg.ml
@@ -7,8 +7,19 @@ let y n = distance n *. sin (float n *. 0.763932 *. Float.pi)
 let header n =
   Printf.printf "<svg viewBox=\"-%d -%d %d %d\" xmlns=\"http://www.w3.org/2000/svg\">" n n (2* n) (2 * n)
 
+let is_prime n =
+  n >= 2 &&
+  let rec p i =
+    i * i > n || (n mod i <> 0 && p (i + 1))
+  in
+  p 2
+
+let color n =
+  if is_prime n then "red"
+  else "#1f76e0"
+
 let add_point n =
-  Printf.printf "<circle cx=\"%f\" cy=\"%f\" r=\"1\" stroke-width=\"0.1\" stroke=\"black\" fill=\"red\">\n  <animateTransform attributeName=\"transform\" attributeType=\"XML\" type=\"rotate\" from=\"0\" to=\"%d\" begin=\"0s\" dur=\"1200s\" repeatCount=\"indefinite\"/>\n</circle>" (x n) (y n) (n * 360)
+  Printf.printf "<circle cx=\"%f\" cy=\"%f\" r=\"1\" stroke-width=\"0.1\" stroke=\"black\" fill=\"%s\">\n  <animateTransform attributeName=\"transform\" attributeType=\"XML\" type=\"rotate\" from=\"0\" to=\"%d\" begin=\"0s\" dur=\"3600s\" repeatCount=\"indefinite\"/>\n</circle>" (x n) (y n) (color n) (n * 360)
 
 let footer () =
   Printf.printf "</svg>"
