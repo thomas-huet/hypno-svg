d6bd683168e182a179b841dc6adda0a331d0464e
Thomas HUET <git@thomash.fr>
2019-02-13 12:00:17 +0100

    More elaborate color scheme

--- make_svg.ml
+++ make_svg.ml
@@ -7,16 +7,29 @@ let y n = distance n *. sin (float n *. 0.763932 *. Float.pi)
 let header n =
   Printf.printf "<svg viewBox=\"-%d -%d %d %d\" xmlns=\"http://www.w3.org/2000/svg\">" n n (2* n) (2 * n)
 
-let is_prime n =
-  n >= 2 &&
-  let rec p i =
-    i * i > n || (n mod i <> 0 && p (i + 1))
+let factors n =
+  let rec f n i =
+    if n mod i = 0 then
+      let a, b, c = f (n / i) i in
+      1 + a, b, c
+    else if n = 1 then
+      0, 0, 0
+    else if i * i > n then
+      0, 1, 1
+    else
+      let a, b, c = f n (i + 1) in
+      0, b + a, c + if a = 0 then 0 else 1
   in
-  p 2
+  let a, b, c = f n 2 in
+  b + a, c + if a = 0 then 0 else 1
+
+let norm n =
+  if n = 0 then 255
+  else 255 / n
 
 let color n =
-  if is_prime n then "red"
-  else "#1f76e0"
+  let a, b = factors n in
+  Printf.sprintf "#%02x00%02x" (norm a) (255 - norm b)
 
 let add_point n =
   Printf.printf "<circle cx=\"%f\" cy=\"%f\" r=\"1\" stroke-width=\"0.1\" stroke=\"black\" fill=\"%s\">\n  <animateTransform attributeName=\"transform\" attributeType=\"XML\" type=\"rotate\" from=\"0\" to=\"%d\" begin=\"0s\" dur=\"3600s\" repeatCount=\"indefinite\"/>\n</circle>" (x n) (y n) (color n) (n * 360)
